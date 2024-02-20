7938456cb9312d3babe314add814b612feb684e1
virinchikvl <virinchikvl9@gmail.com>
2024-02-20 17:33:53 +0530

    arrays

---
+++ function.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE() {
+if [ $? -ne 0 ]
+then
+    echo "Installation of mysql is error"
+    exit 1
+else
+    echo "Installation of mysql is success"
+fi
+}
+if [USERID -ne 0]
+	then 
+		echo "run this script with root access"
+		exit 1
+   else 
+		echo "INFO :: Your istallation is in process"
+fi 
+
+yum install mysql -y
+
+VALIDATE $?
+
+yum install mysql -y
+
+VALIDATE $?
