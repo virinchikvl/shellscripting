83f5f5ab0f3871fd7af9ce9aa8c463187dcfb52e
virinchikvl <virinchikvl9@gmail.com>
2024-02-20 12:27:22 +0530

    1st commit

---
+++ conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# check if the number is greater than 10 or not
+read enter your number:NUMBER
+NUMBER=$1
+
+if [ $NUMBER -gt 10 ]
+then
+    echo "$NUMBER is greater than 10"
+else
+    echo "$NUMBER is not greater than 10"
+fi
