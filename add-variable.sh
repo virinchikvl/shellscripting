d90ab742d6e7bf995cb6dcc9ec178510a707d6e1
virinchikvl <virinchikvl9@gmail.com>
2024-02-20 12:48:52 +0530

    1st commit

---
+++ add-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Take input from user and calculate sum.
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+ 
+# Calculate sum
+sum=$(( $num1 + $num2 ))
+
+# Display the result
+echo "Sum is: $sum"   
