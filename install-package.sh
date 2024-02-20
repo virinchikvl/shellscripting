11cf4a058f2c78b459903a25954eb26beda24d51
virinchikvl <virinchikvl9@gmail.com>
2024-02-20 17:06:40 +0530

    arrays

--- install-package.sh
+++ install-package.sh
@@ -7,7 +7,25 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "run this script with root access"
-else 
-    echo "INFO :: Your istallation is in process"
+    exit 1
+#else 
+#    echo "INFO :: Your istallation is in process"
 fi 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "run this script with root access"
+exit 1
+else 
+    echo "INFO :: Your istallation is in process"
+fi
+
+yum install postfix -y 
+if [ $? -ne 0 ]
+then 
+    echo "run this script with root access"
+exit 1
+else 
+    echo "INFO :: Your istallation is in process"
+fi
