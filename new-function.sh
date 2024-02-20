99bf2db01c75b5b65fba9b16626aece9edc26d52
virinchikvl <virinchikvl9@gmail.com>
2024-02-20 19:07:53 +0530

    arrays

--- new-function.sh
+++ new-function.sh
@@ -22,7 +22,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR:: Please run this script with root access"
+    echo "ERROR:: Please run this script with root access" 
     exit 1
 # else
 #     echo "INFO:: You are root user"
