8b8c0da0c41ef84e6123114e6d76849a33fb1eec
virinchikvl <virinchikvl9@gmail.com>
2024-02-21 10:59:35 +0530

    arrays

--- loops-arguments.sh
+++ loops-arguments.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+USERID=$(id -u)
+
+if  [ $USERID -ne 0 ]
+then 
+		echo "ERROR :: Run this with root user "
+		exit 1
+fi 
+
   # $@ will take all values giving along with script 
 for i in $@
 do 
